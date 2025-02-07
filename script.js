f36f06bbf9962e54d886bdcd13404532adad9b02
Aleksa Ilic <aleksailicdev@gmail.com>
2025-02-07 07:44:12 +0100

    Update script.js

--- script.js
+++ script.js
@@ -15,18 +15,3 @@ function result() {
     display.value = "Error";
   }
 }
-// function toDisplay(value) {
-//   document.getElementById("display").value += value;
-// }
-// function clearDisplay() {
-//   document.getElementById("display").value = "";
-// }
-// function result() {
-//   try {
-//     document.getElementById("display").value = eval(
-//       document.getElementById("display").value
-//     );
-//   } catch {
-//     document.getElementById("display").value = "Error";
-//   }
-// }
